43f223924355727aec7c2c66d3c99a0e83e8d0e2
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-07-23 02:55:24 +1000

    6. Empty Resource

--- Framework/QueryResource.cs
+++ Framework/QueryResource.cs
@@ -1,37 +1,14 @@
-﻿//using System.Collections.Generic; 
-//using System.Linq; 
-//using Dapper;
-//using Glimpse.Core.Extensibility;
-//using Glimpse.Core.Extensions;
-//using Glimpse.Core.Framework;
-//using Glimpse.Core.ResourceResult;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dapper;
+using Glimpse.Core.Extensibility;
+using Glimpse.Core.Extensions;
+using Glimpse.Core.Framework;
+using Glimpse.Core.ResourceResult;
 
-//namespace MvcMusicStore.Framework
-//{
-//    public class QueryResource : IResource
-//    {
-//        private const string QueryKey = "query";
-
-//        public string Name
-//        {
-//            get { return "music_query"; }
-//        }
-
-//        public IEnumerable<ResourceParameterMetadata> Parameters
-//        {
-//            get { return new[] { new ResourceParameterMetadata(QueryKey) }; }
-//        }
-
-//        public IResourceResult Execute(IResourceContext context)
-//        {
-//            var queryValue = context.Parameters.GetValueOrDefault(QueryKey);
-             
-//            using (var connection = SqlMapper.CreateConnection())
-//            { 
-//                var data = connection.Query(queryValue).ToList();
-
-//                return new CacheControlDecorator(0, CacheSetting.NoCache, new JsonResourceResult(data, null));
-//            }
-//        }
-//    }
-//}
+namespace MvcMusicStore.Framework
+{
+    public class QueryResource : IResource
+    {
+    }
+}
