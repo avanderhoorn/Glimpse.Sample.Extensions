7ab3c441190faf88f0ec7b20191d9ecc2b718955
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-07-23 03:10:08 +1000

    6. Include client script

--- Framework/QueryClientScript.cs
+++ Framework/QueryClientScript.cs
@@ -1,17 +1,8 @@
-﻿//using Glimpse.Core.Extensibility;
+﻿using Glimpse.Core.Extensibility;
 
-//namespace MvcMusicStore.Framework
-//{
-//    public class QueryClientScript : IStaticClientScript
-//    {
-//        public ScriptOrder Order
-//        {
-//            get { return ScriptOrder.IncludeAfterClientInterfaceScript; }
-//        }
-
-//        public string GetUri(string version)
-//        {
-//            return "/Framework/QueryScript.js";
-//        }
-//    }
-//}
+namespace MvcMusicStore.Framework
+{
+    public class QueryClientScript : IStaticClientScript
+    {
+    }
+}
--- Framework/QueryScript.js
+++ Framework/QueryScript.js
@@ -1,3 +1,3 @@
-﻿(function ($, pubsub, util, data, renderEngine) { 
-    
+﻿(function ($, pubsub, util, data, renderEngine) {
+    alert('test');
 })(jQueryGlimpse, glimpse.pubsub, glimpse.util, glimpse.data, glimpse.render.engine);
