80a3cbde25ff31d49357245dbdf997a19b9f971a
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-07-23 01:45:32 +1000

    1. Empty Tab

--- Framework/ConfigurationTab.cs
+++ Framework/ConfigurationTab.cs
@@ -1,21 +1,12 @@
-﻿//using Glimpse.AspNet.Extensibility;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
+﻿using Glimpse.AspNet.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
-//namespace MvcMusicStore.Framework
-//{
-//    public class ConfigurationTab : AspNetTab
-//    {
-//        public override string Name
-//        {
-//            get { return "Setup"; }
-//        }
-
-//        public override object GetData(Glimpse.Core.Extensibility.ITabContext context)
-//        {
-//            return MvcMusicStore.Models.Configuration.Current;
-//        }
-//    }
-//}
+namespace MvcMusicStore.Framework
+{
+    public class ConfigurationTab : AspNetTab
+    {
+    }
+}
