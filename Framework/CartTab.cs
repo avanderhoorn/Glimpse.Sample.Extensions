7e31571b91b41228a2747269c7442e79216a2acf
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-07-23 02:09:44 +1000

    2. Added base layout

--- Framework/CartTab.cs
+++ Framework/CartTab.cs
@@ -7,8 +7,22 @@ using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Framework
 {
-    public class CartTab : AspNetTab
-    { 
+    public class CartTab : AspNetTab, ITabLayout
+    {
+        private static readonly object Layout = TabLayout.Create()
+            .Row(r =>
+            {
+                r.Cell("albumTitle").WithTitle("Album");
+                r.Cell("albumId").WithTitle("Id");
+                r.Cell("albumPrice").AlignRight().WidthInPixels(100).WithTitle("Price");
+                r.Cell("genreName").WithTitle("Genre");
+                r.Cell("artistName").WithTitle("Artist");
+                r.Cell("count").Class("mono").WidthInPixels(70).WithTitle("Count");
+                r.Cell("dateCreated").WithTitle("Added");
+                r.Cell("recordId").WithTitle("Record Id");
+                r.Cell("cartId").WithTitle("Cart Id");
+            }).Build();
+
         public override string Name
         {
             get { return "Cart"; }
@@ -27,6 +41,11 @@ namespace MvcMusicStore.Framework
         public override RuntimeEvent ExecuteOn
         {
             get { return RuntimeEvent.EndSessionAccess; }
+        }
+
+        public object GetLayout()
+        {
+            return Layout;
         }
     }
 }
