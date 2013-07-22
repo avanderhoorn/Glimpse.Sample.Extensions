1fd926ce5387475c5b4170d381abbecd37b44195
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-07-23 03:28:54 +1000

    7. Stub resource

--- Framework/InventoryResource.cs
+++ Framework/InventoryResource.cs
@@ -14,5 +14,25 @@ namespace MvcMusicStore.Framework
 {
     public class InventoryResource : IResource
     {
+        private const string AlbumIdKey = "albumId";
+
+        public string Name
+        {
+            get { return "music_query"; }
+        }
+
+        public IEnumerable<ResourceParameterMetadata> Parameters
+        {
+            get { return new[] { new ResourceParameterMetadata(AlbumIdKey) }; }
+        }
+
+        public IResourceResult Execute(IResourceContext context)
+        {
+            var queryValue = int.Parse(context.Parameters.GetValueOrDefault(AlbumIdKey));
+
+            var data = InventoryManager.GetInventory(queryValue);
+
+            return new CacheControlDecorator(0, CacheSetting.NoCache, new JsonResourceResult(data, null)); ;
+        }
     }
 }
