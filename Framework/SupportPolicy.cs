d128b5819de5064ea82a329676e6b59eaea5799c
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-07-23 02:38:46 +1000

    5. Base policy

--- Framework/SupportPolicy.cs
+++ Framework/SupportPolicy.cs
@@ -9,5 +9,20 @@ namespace MvcMusicStore.Framework
 {
     public class SupportPolicy : IRuntimePolicy
     {
+        public RuntimePolicy Execute(IRuntimePolicyContext policyContext)
+        {
+            var httpContext = policyContext.GetHttpContext();
+            if (!httpContext.User.IsInRole("Administrator"))
+            {
+                return RuntimePolicy.Off;
+            }
+
+            return RuntimePolicy.On;
+        }
+
+        public RuntimeEvent ExecuteOn
+        {
+            get { return RuntimeEvent.EndRequest; }
+        }
     }
 }
